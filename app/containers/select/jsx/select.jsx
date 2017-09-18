8cb78b57aad8bc5310d609b4df8de5872f92f9cc
forgotQ <zbj987@hotmail.com>
2017-09-18 23:39:30 +0800

    changed

--- app/containers/select/jsx/select.jsx
+++ app/containers/select/jsx/select.jsx
@@ -4,18 +4,18 @@ import '../style/select.sass'
 class Zselect extends React.Component{
     constructor(props){
         super(props)
-        this.setState = {
+        this.state = {
             open:false
         }
     }
     render(){
         return(
-            <div className="Zselect-wrapper" tabIndex='0'>
+            <div className="Zselect-wrapper" tabIndex='0' onClick={this.selectChanged.bind(this)}>
                 <span className="Zselect-selected">选择框选择</span>
-                <svg className="Zselect-moreunfold" aria-hidden="true" onClick={this.selectChanged.bind(this)}>
+                <svg className="Zselect-moreunfold" aria-hidden="true">
                     <use xlinkHref="#icon-moreunfold"></use>
                 </svg>
-                <ul className="Zselect-selected-item">
+                <ul className={"Zselect-selected-item  "+this.state.open?"":"Z-none"}>
                     <li className="Zselect-selected-item-li">第一个</li>
                     <li className="Zselect-selected-item-li">第一个</li>
                     <li className="Zselect-selected-item-li">第一个</li>
@@ -23,7 +23,7 @@ class Zselect extends React.Component{
             </div>
         )
     }
-    selectChanged() {
+    selectChanged(e) {
 
     }
 }export default Zselect
