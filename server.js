4d0aae9efcf29107d09872822310b57356bafaa8
forgotQ <zbj987@hotmail.com>
2018-01-14 18:13:59 +0800

    'commit'

--- server.js
+++ server.js
@@ -6,28 +6,28 @@ var webpack = require('webpack');
 var WebpackDevServer = require('webpack-dev-server');
 var config = require('./webpack.dev');
 var compiler = webpack(config);
-// var webpackDevMiddleware = require('webpack-dev-middleware');
-// var WebpackHotMiddleware = require('webpack-hot-middleware')
-// var app = express();
-// app.use(webpackDevMiddleware(compiler,
-//     {
-//     publicPath: config.output.publicPath,
-//     hot: true,
-//     index :"index.tmpl.html",
-//     historyApiFallback: true,
-//     inline: true,
-//     proxy: {
-//         '/api': {
-//             target: 'http://localhost:3000',
-//             changeOrigin: true,
-//             secure: false
-//         }
-//     },
-// }));
-// app.use(WebpackHotMiddleware(compiler));
-// app.listen(8080,function (err, result) {
-//
-// });
+var webpackDevMiddleware = require('webpack-dev-middleware');
+var WebpackHotMiddleware = require('webpack-hot-middleware')
+var app = express();
+app.use(webpackDevMiddleware(compiler,
+    {
+    publicPath: config.output.publicPath,
+    hot: true,
+    index :"index.tmpl.html",
+    historyApiFallback: true,
+    inline: true,
+    proxy: {
+        '/api': {
+            target: 'http://localhost:3000',
+            changeOrigin: true,
+            secure: false
+        }
+    },
+}));
+app.use(WebpackHotMiddleware(compiler));
+app.listen(8080,function (err, result) {
+
+});
 new WebpackDevServer(compiler, {
     publicPath: config.output.publicPath,
     hot: true,
@@ -40,6 +40,6 @@ new WebpackDevServer(compiler, {
             secure: false
         }
     },
-}).listen(8081, 'localhost', function (err, result) {
+}).listen(8082, 'localhost', function (err, result) {
 
 });
